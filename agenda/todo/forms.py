75be4b7136d61332cf8855d3a9530b4d3ff2b25e
AlexIakh <111706573+AlexIakh@users.noreply.github.com>
2023-02-20 10:23:07 +0100

    Add files via upload

---
+++ agenda/todo/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm, DateInput
+from .models import Todo
+
+class TodoForm(ModelForm):
+    class Meta:
+        model = Todo
+        exclude = ('date',)
+        widget = {
+            'estimated_end': DateInput(attrs={'type': 'date'}),
+            }
